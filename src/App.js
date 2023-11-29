9e9599f4b4bb27c978f98731e98394fddc4339ce
Pradeepkumar1407 <pk969989@gmail.com>
2023-11-29 09:48:25 +0530

    Pricing Example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from "./Card";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
 
 function App() {
+  var priceCards = [
+    {
+      plan: "Free",
+      price: "0",
+      buttonoutline: true,
+      features: [
+        {
+          title: "10 users included",
+        },
+        {
+          title: "2 GB of storage",
+        },
+        {
+          title: "Email support",
+        },
+        {
+          title: "Help center access"
+        }
+
+      ]
+    },
+    {
+      plan: "Pro",
+      price: "10",
+      buttonoutline: false,
+      features: [
+        {
+          title: "30 users included",
+        },
+        {
+          title: "10 GB of storage",
+        },
+        {
+          title: "Priority Email support",
+        },
+        {
+          title: "Help center access"
+        }
+
+      ]
+
+    },
+    {
+      plan: "Enterprise",
+      price: "20",
+      highlight: true,
+      buttonoutline: false,
+      features: [
+        {
+          title: "40 users included",
+        },
+        {
+          title: "15 GB of storage",
+        },
+        {
+          title: "Phone And Email support",
+        },
+        {
+          title: "Help center access"
+        }
+
+      ]
+    }
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row mt-3">
+        {
+          priceCards.map((card) => {
+            return (<Card card={card} />)
+          })
+        }
+      </div>
     </div>
   );
 }
